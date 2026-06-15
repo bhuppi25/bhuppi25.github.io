b6a6ef1a089001bced5c12e67ee65c77a6871896
Bhupendra Singh <bhupendraonjob@gmail.com>
2026-06-15 12:08:03 +0530

    Fixed config changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
+const isGitHubPagesDeploy = process.env.DEPLOY_TARGET === "gh-pages";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/bhuppi25.github.io/',
+  base: isGitHubPagesDeploy ? '/bhuppi25.github.io/' : '/',
 })
